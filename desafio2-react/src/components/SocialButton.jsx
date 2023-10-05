45e095e186b70050782f577cb57f9935b156513c
Josetv4 <joseflauta1993@gmail.com>
2023-10-05 12:50:35 -0300

    se dio formato al componente

--- desafio2-react/src/components/SocialButton.jsx
+++ desafio2-react/src/components/SocialButton.jsx
@@ -1,9 +1,12 @@
-
-
-const SocialButton = ({url, icon}) => {
+const SocialButton = ({ url, icon }) => {
     return (
         <div className="socialButton">
-            <a href={url} target="_blank" class="btn btn-lg btn-outline-dark rounded-circle" role="button">
+            <a
+                href={url}
+                target="_blank"
+                class="btn btn-lg btn-outline-dark rounded-circle"
+                role="button"
+            >
                 <i class={icon}></i>
             </a>
         </div>
